376a55d2b45bccf3cf4fa994e8d15d426b6292fb
Yakiv Khorunzhyi <63973781+yakiv-khorunzhyi@users.noreply.github.com>
2024-10-13 14:55:20 +0300

    Add swagger annotation package, request examples, successful responses to swagger, fix documentation link

---
+++ app/Http/Controllers/ApiDocumentationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="0.0.1",
+ *      title="E-Signature API",
+ *      description="Basic E-Signature API using Laravel that allows users to e-sign documents.",
+ *      @OA\Contact(
+ *          email="yakiv.khorunzhyi@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+class ApiDocumentationController extends Controller
+{
+}
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,42 @@ class AuthController extends Controller
 {
     use ApiResponse;
 
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     summary="Register user",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name", "email", "password"},
+     *             @OA\Property(property="name", type="string", example="Yakiv Khorunzhyi"),
+     *             @OA\Property(property="email", type="string", format="email", example="yakiv.khorunzhyi@gmail.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password"),
+     *             @OA\Property(property="password_confirmation", type="string", format="password", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Registered",
+     *         @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(
+     *                      property="user",
+     *                      type="object",
+     *                      @OA\Property(property="id", type="integer", example=4),
+     *                      @OA\Property(property="name", type="string", example="Yakiv Khorunzhyi"),
+     *                      @OA\Property(property="email", type="string", example="yakiv.khorunzhyi@gmail.com"),
+     *                      @OA\Property(property="created_at", type="string", format="date-time", example="2024-10-13T11:31:32.000000Z"),
+     *                      @OA\Property(property="updated_at", type="string", format="date-time", example="2024-10-13T11:31:32.000000Z")
+     *                  )
+     *              )
+     *          )
+     *     )
+     * )
+     */
     public function register(RegisterRequest $request, RegisterAction $action): JsonResponse
     {
         $actionResult = $action->execute(
@@ -24,6 +60,32 @@ class AuthController extends Controller
         return $this->responseCreated(['user' => $actionResult->getData()]);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Login user",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"email","password"},
+     *             @OA\Property(property="email", type="string", format="email", example="yakiv.khorunzhyi@gmail.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful login",
+     *         @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(property="token", type="string", example="2|zSNFl1lPnucDwbgFTuylT1nc7cjRtMDrUjRW1eys88f2682a")
+     *              )
+     *          )
+     *     )
+     * )
+     */
     public function login(LoginRequest $request, LoginAction $action): JsonResponse
     {
         $actionResult = $action->execute(
